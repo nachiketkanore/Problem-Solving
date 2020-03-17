d35df59800664ce9bba26ab3b823c1880ad3e664
nachiketkanore <44920607+nachiketkanore@users.noreply.github.com>
2020-03-17 22:27:38 +0530

    Update Knapsack-1.cpp

--- Atcoder-DP/Knapsack-1.cpp
+++ Atcoder-DP/Knapsack-1.cpp
@@ -1,10 +1,46 @@
-//Top-down with memoization
+//1.Top-down with memoization (AC)
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int inf = 1e12;
+const int N = 105;
+const int W = 1e5 + 5;
+
+int n, total, weight[N], value[N], dp[N][W];
+
+int solve(int item, int remain){
+	if(remain < 0)
+		return -inf;
+	if(remain == 0 || item == 0)
+		return 0;
+
+	int &ans = dp[item][remain];
+	if(ans != 0)
+		return ans;
+
+	int take = value[item] + solve(item - 1, remain - weight[item]);
+	int dont_take = solve(item - 1, remain);
+		
+	ans = max(take, dont_take);
+
+	return ans;
+}
+
+int32_t main(){ 
+
+	cin >> n >> total;
+	for(int i = 1; i <= n; i++)
+		cin >> weight[i] >> value[i];
+
+	cout << solve(n, total) << '\n';
+    
+}
 
 
-//Recursive without memoization
+//2.Recursive without memoization (TLE)
 
-
-//It's time to be who I am, rather than who I am supposed to be.
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
@@ -26,14 +62,7 @@ int solve(int item, int remain){
 }
 
 int32_t main(){ 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
-
+	
 	cin >> n >> total;
 	for(int i = 1; i <= n; i++)
 		cin >> weight[i] >> value[i];
